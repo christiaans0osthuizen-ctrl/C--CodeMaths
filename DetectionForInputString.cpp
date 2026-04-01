dfb5ae9863f1c1cfa56c53287b717599f4c55fd8
Elemental_Coding <271999899+christiaans0osthuizen-ctrl@users.noreply.github.com>
2026-04-01 22:14:52 +0200

    fixed swich case study

--- DetectionForInputString.cpp
+++ DetectionForInputString.cpp
@@ -1,28 +1,10 @@
 #include <iostream>
 #include <string>
-/*
-int main(){
-    std::string names[3];
-    int scores[9];
 
-        for(int i = 0; i < 3; i++){
-            std::cout<<"Name: ";
-            getline(std::cin, names[i]);
-            std::cout<<"Score: ";
-            std::cin>>scores[i];
-            std::cin.ignore();
-        }
-        for(int i = 0; i < 3; i++){
-            std::cout<<names[i]<<'\n';
-            std::cout<<std::to_string(scores[i])<<std::endl;
-        }
-    
-    return 0;
-}
-    */
 
 int main(){
     std::string names[3];
+    std::string scores_string[9];
     int scores[9];
 
         for(int i = 0; i < 3; i++){
@@ -32,45 +14,51 @@ int main(){
                 case 0:
                     for(int x = 0; x < 3; x++){
                         std::cout<<"Score: ";
-                        std::cin>>scores[x]; 
+                        std::cin>>scores[x];
+                        std::cin.ignore(); 
                     }
                 break;
                 case 1:
                     for(int x = 3; x < 6; x++){
                         std::cout<<"Score: ";
                         std::cin>>scores[x];
+                        std::cin.ignore(); 
                     }
                 break;
                 case 2:
                     for(int x = 6; x < 9; x++){
                         std::cout<<"Score: ";
                         std::cin>>scores[x];
+                        std::cin.ignore(); 
                     }
                 break;
             }
             
             
         }
-        for(int i = 0; i < 3; i++){
-            std::cout<<names[i]<<'\n';
-              switch (i){
-                case 0:
-                    for(int x = 0; x < 3; x++){
-                        std::cout<<"Score: " + scores[x];
-                    }
-                break;
-                case 1:
-                    for(int x = 3; x < 6; x++){
-                        std::cout<<"Score: " + scores[x];
-                    }
-                break;
-                case 2:
-                    for(int x = 6; x < 9; x++){
-                        std::cout<<"Score: " + scores[x];
-                    }
-                break;
-            }
+ for(int i = 0; i < 9; i++){
+        scores_string[i] = std::to_string(scores[i]);
+    }
+
+    std::cout<<"======================="<<std::endl;
+    for(int i = 0; i < 3; i++){
+    std::cout<<names[i]<<std::endl;
+        switch (i){
+            case 0:
+                for(int x = 0; x < 3; x++){
+                    std::cout<<"Score: " + scores_string[x]<<std::endl;
+                }
+            break;
+            case 1:
+                for(int x = 3; x < 6; x++){
+                    std::cout<<"Score: " + scores_string[x]<<std::endl;
+                }
+            break;
+            case 2:
+                for(int x = 6; x < 9; x++){
+                    std::cout<<"Score: " + scores_string[x]<<std::endl;
+                }
+            break;
+            } 
         }
-    
-    return 0;
 }
