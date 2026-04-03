a5122171ae4a9e37babe0db4eb7016ec0f8e4ab7
Elemental_Coding <271999899+christiaans0osthuizen-ctrl@users.noreply.github.com>
2026-04-03 23:10:39 +0200

    Temp changes new aprouce

--- DetectionForInputString.cpp
+++ DetectionForInputString.cpp
@@ -1,64 +1,44 @@
 #include <iostream>
 #include <string>
 
+/*
+i*3 + x
+
+0*3 + 0 = 0     
+0*3 + 1 = 1
+0*3 + 2 = 2
+
+1*3 + 0 = 3
+1*3 + 1 = 4
+1*3 + 2 = 5
+
+2*3 + 0 = 6
+2*3 + 1 = 7
+2*3 + 2 = 8
+*/
+
 
 int main(){
     std::string names[3];
-    std::string scores_string[9];
     int scores[9];
 
         for(int i = 0; i < 3; i++){
-            std::cout<<"Name: ";
-            getline(std::cin, names[i]);
-            switch (i){
-                case 0:
-                    for(int x = 0; x < 3; x++){
-                        std::cout<<"Score: ";
-                        std::cin>>scores[x];
-                        std::cin.ignore(); 
+            std::cout<< "\033[1;37;41mName[" << (i + 1) << "]:\033[0m ";
+            std::getline(std::cin, names[i]);
+            for(int x = 0; x < 3; x++){
+                bool input_is_number = false;
+                do{
+                    std::cout<< "\033[1;37;41mScore[" << (x + 1) << "]:\033[0m ";
+                    if(std::cin>> scores[i*3 + x]){
+                        input_is_number = true;
                     }
-                break;
-                case 1:
-                    for(int x = 3; x < 6; x++){
-                        std::cout<<"Score: ";
-                        std::cin>>scores[x];
-                        std::cin.ignore(); 
+                    else{
+                        std::cout<<"\033[1;37;41mInput Invailid, Please try again\033[0m" <<'\n';
+                        std::cin.clear();
+                        std::cin.ignore(1000, '\n');
                     }
-                break;
-                case 2:
-                    for(int x = 6; x < 9; x++){
-                        std::cout<<"Score: ";
-                        std::cin>>scores[x];
-                        std::cin.ignore(); 
-                    }
-                break;
+                }while(!input_is_number);
             }
-            
-            
         }
- for(int i = 0; i < 9; i++){
-        scores_string[i] = std::to_string(scores[i]);
-    }
-
-    std::cout<<"======================="<<std::endl;
-    for(int i = 0; i < 3; i++){
-    std::cout<<names[i]<<std::endl;
-        switch (i){
-            case 0:
-                for(int x = 0; x < 3; x++){
-                    std::cout<<"Score: " + scores_string[x]<<std::endl;
-                }
-            break;
-            case 1:
-                for(int x = 3; x < 6; x++){
-                    std::cout<<"Score: " + scores_string[x]<<std::endl;
-                }
-            break;
-            case 2:
-                for(int x = 6; x < 9; x++){
-                    std::cout<<"Score: " + scores_string[x]<<std::endl;
-                }
-            break;
-            } 
-        }
+    return 0;
 }
